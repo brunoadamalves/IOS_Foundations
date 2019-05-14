19b84f8cadab86c598e6569c631e75bd2592119b
Bruno <bruno.adamm@gmail.com>
2019-05-14 15:53:38 -0300

    aula7

--- Aula 7/MultiPageApp/MultiPageApp/Controller/ViewController.swift
+++ Aula 7/MultiPageApp/MultiPageApp/Controller/ViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var opcao = 0
 
     @IBOutlet weak var emailButton: UIButton!
     @IBOutlet weak var facebookButton: UIButton!
     @IBOutlet weak var googlePlusButton: UIButton!
-    
+    @IBAction func emailButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func facebookButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func googlePlusButtonAction(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         emailButton.layer.cornerRadius = 25
         facebookButton.layer.cornerRadius = 25
         googlePlusButton.layer.cornerRadius = 25
     }
+    
+    
 }
 
