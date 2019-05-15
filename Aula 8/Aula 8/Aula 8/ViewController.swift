e634b49b07139976fad31fac2fb1e139f66c4916
Bruno <bruno.adamm@gmail.com>
2019-05-15 11:16:27 -0300

    Nano Challange
    First commit

--- Aula 8/Aula 8/Aula 8/ViewController.swift
+++ Aula 8/Aula 8/Aula 8/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Person {
+    var name: String
+    var age: Int
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
+    @IBOutlet weak var tablewView: UITableView!
+    
+    var people: [Person] =
+    [
+        Person(name: "Teste", age: 20),
+        Person(name: "Teste 2", age: 21),
+        Person(name: "Teste 3", age: 25)
+    ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell else { fatalError()}
+        
+        let person = people[indexPath.row]
+        
+        cell.nameLabel.text = person.name
+        cell.ageLabel.text = String(person.age)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tablewView.dataSource = self
+        tablewView.delegate = self
     }
 
 
