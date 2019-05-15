882b6246f675710018e15b8fbdc5e6e400651484
Bruno <bruno.adamm@gmail.com>
2019-05-15 11:16:03 -0300

    Nano Challange
    First commit

---
+++ Aula 8/Aula 8/Aula 8/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Aula 8
+//
+//  Created by Bruno Adam Alves on 15/05/19.
+//  Copyright © 2019 Bruno Adam Alves. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
