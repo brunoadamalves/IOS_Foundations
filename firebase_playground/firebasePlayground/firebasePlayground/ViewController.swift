d8582d82a698d7f8e5508a70ffa2a5cc7d170136
Bruno <bruno.adamm@gmail.com>
2019-06-05 12:43:23 -0300

    WIP
    WIP

---
+++ firebase_playground/firebasePlayground/firebasePlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebasePlayground
+//
+//  Created by Bruno Adam Alves on 31/05/19.
+//  Copyright © 2019 Bruno Adam Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
